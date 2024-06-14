b1845680b0faa168aad97d92dd21941c8b95b6ab
Onyeamah Tochukwu <tochinicky@gmail.com>
2024-06-14 19:28:18 +0100

    published domain events after successful transaction on the database

--- src/Bookify.Infrastructure/ApplicationDbContext.cs
+++ src/Bookify.Infrastructure/ApplicationDbContext.cs
@@ -1,18 +1,49 @@
 ﻿using Bookify.Domain;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Infrastructure;
 
 public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
-    public ApplicationDbContext(DbContextOptions options) : base(options)
+    private readonly IPublisher _publisher; // inject it from mediator
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher) : base(options)
     {
-
+        _publisher = publisher;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
+    }
+
+    //publishing domain events using EF
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEventsAsync();
+        return result;
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        var domainEvents = ChangeTracker
+        .Entries<Entity>()
+        .Select(entry => entry.Entity)
+        .SelectMany(entity =>
+        {
+            var domainEvents = entity.GetDomainEvents();
+            entity.ClearDomainEvents();
+            return domainEvents;
+        })
+        .ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
+
     }
 }
