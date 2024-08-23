ec82ba4884e5bf2f8ac4d09e3e5ade5a02de624b
Onyeamah Tochukwu <tochinicky@gmail.com>
2024-08-23 23:29:49 +0100

    added postgres db

--- src/Bookify.Api/Program.cs
+++ src/Bookify.Api/Program.cs
@@ -23,7 +23,7 @@ builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI(options =>
